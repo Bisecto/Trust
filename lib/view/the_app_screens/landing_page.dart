5cd27c5afa65b09c34b2dfc4a9918201cfe7e048
precious <bisector0@gmail.com>
2024-08-29 03:31:31 +0100

    update:Foreground notification fixed

--- lib/view/the_app_screens/landing_page.dart
+++ lib/view/the_app_screens/landing_page.dart
@@ -13,6 +13,7 @@ import 'package:teller_trust/res/app_icons.dart';
 import 'package:teller_trust/view/important_pages/no_internet.dart';
 import 'package:teller_trust/view/sendBeneficary/pages/send_main_page.dart';
 import 'package:teller_trust/view/the_app_screens/send_page.dart';
+import 'package:teller_trust/view/widgets/show_toast.dart';
 
 import '../../bloc/app_bloc/app_bloc.dart';
 import '../../domain/txn/txn_details_to_send_out.dart';
@@ -21,6 +22,7 @@ import '../../res/app_colors.dart';
 import '../../utills/app_navigator.dart';
 import '../../utills/app_utils.dart';
 import '../../utills/custom_theme.dart';
+import '../../utills/enums/toast_mesage.dart';
 import '../../utills/shared_preferences.dart';
 import '../auth/sign_in_with_access_pin_and_biometrics.dart';
 import '../sendBeneficary/pages/send_to_page.dart';
@@ -56,29 +58,33 @@ class _LandingPageState extends State<LandingPage> {
         setState(() {
           isNotification = true;
         });
+        showToast(context: context,
+            title: notification.title!,
+            subtitle: notification.body!,
+            type: ToastMessageType.info);
 
-        modalSheet.showMaterialModalBottomSheet(
-          context: context,
-          enableDrag: true,
-          isDismissible: true,
-          expand: false,
-          builder: (context) => SingleChildScrollView(
-            controller: modalSheet.ModalScrollController.of(context),
-            child: Padding(
-              padding: const EdgeInsets.all(0.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    width: AppUtils.deviceScreenSize(context).width,
-                    height: 100,
-                    color: AppColors.blue,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
+        // modalSheet.showMaterialModalBottomSheet(
+        //   context: context,
+        //   enableDrag: true,
+        //   isDismissible: true,
+        //   expand: false,
+        //   builder: (context) => SingleChildScrollView(
+        //     controller: modalSheet.ModalScrollController.of(context),
+        //     child: Padding(
+        //       padding: const EdgeInsets.all(0.0),
+        //       child: Column(
+        //         crossAxisAlignment: CrossAxisAlignment.start,
+        //         children: <Widget>[
+        //           Container(
+        //             width: AppUtils.deviceScreenSize(context).width,
+        //             height: 100,
+        //             color: AppColors.blue,
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //   ),
+        // );
         flutterLocalNotificationsPlugin.show(
             notification.hashCode,
             notification.title,
@@ -165,7 +171,7 @@ class _LandingPageState extends State<LandingPage> {
         Connectivity().onConnectivityChanged.listen(_handleConnectivity);
 
     landPageScreens = [
-       HomePage(onPageChanged: _onPageChanged),
+      HomePage(onPageChanged: _onPageChanged),
       SendToPage(),
       const BillsPage(),
       const CardPage(),
@@ -192,6 +198,7 @@ class _LandingPageState extends State<LandingPage> {
       });
     }
   }
+
   void _onPageChanged(int index) {
     setState(() {
       _currentIndex = index;
@@ -206,144 +213,146 @@ class _LandingPageState extends State<LandingPage> {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Provider.of<CustomThemeState>(context).adaptiveThemeMode;
+    final theme = Provider
+        .of<CustomThemeState>(context)
+        .adaptiveThemeMode;
 
     return _connected
         ? IdleDetector(
-            idleTime: const Duration(minutes: 20),
-            onIdle: () async {
-              String firstame = await SharedPref.getString('firstName');
+      idleTime: const Duration(minutes: 20),
+      onIdle: () async {
+        String firstame = await SharedPref.getString('firstName');
 
-              AppNavigator.pushAndRemovePreviousPages(context,
-                  page: SignInWIthAccessPinBiometrics(
-                    userName: firstame,
-                  ));
-              // Perform actions when the user becomes idle
-            },
-            child: Scaffold(
-              backgroundColor: theme.isDark
-                  ? AppColors.darkModeBackgroundColor
-                  : AppColors.lightShadowGreenColor,
+        AppNavigator.pushAndRemovePreviousPages(context,
+            page: SignInWIthAccessPinBiometrics(
+              userName: firstame,
+            ));
+        // Perform actions when the user becomes idle
+      },
+      child: Scaffold(
+        backgroundColor: theme.isDark
+            ? AppColors.darkModeBackgroundColor
+            : AppColors.lightShadowGreenColor,
 
-              body: IndexedStack(
-                children: [
-                  landPageScreens[_currentIndex],
-                ],
-              ),
-              bottomNavigationBar: BottomNavigationBar(
-                backgroundColor: AppColors.white,
-                showUnselectedLabels: true,
-                currentIndex: _currentIndex,
-                selectedItemColor: AppColors.green,
-                unselectedItemColor: theme.isDark
+        body: IndexedStack(
+          children: [
+            landPageScreens[_currentIndex],
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: AppColors.white,
+          showUnselectedLabels: true,
+          currentIndex: _currentIndex,
+          selectedItemColor: AppColors.green,
+          unselectedItemColor: theme.isDark
+              ? AppColors.lightPrimary
+              : AppColors.lightDivider,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AppIcons.home,
+                color: _currentIndex == 0
+                    ? AppColors.green
+                    : theme.isDark
                     ? AppColors.lightPrimary
-                    : AppColors.lightDivider,
-                onTap: (index) {
-                  setState(() {
-                    _currentIndex = index;
-                  });
-                },
-                items: [
-                  BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      AppIcons.home,
-                      color: _currentIndex == 0
-                          ? AppColors.green
-                          : theme.isDark
-                              ? AppColors.lightPrimary
-                              : AppColors.lightgrey,
-                    ), //Icon(Icons.home),
-                    label: 'Home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: SvgPicture.asset(AppIcons.send,
-                        color: _currentIndex == 1
-                            ? AppColors.green
-                            : theme.isDark
-                                ? AppColors.lightPrimary
-                                : AppColors.lightgrey),
-                    label: 'Send',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      AppIcons.bill,
-                      color: _currentIndex == 2
-                          ? AppColors.green
-                          : theme.isDark
-                              ? AppColors.lightPrimary
-                              : AppColors.lightgrey,
-                    ), //Icon(Icons.home),
-                    label: 'Bills',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      AppIcons.card,
-                      color: _currentIndex == 3
-                          ? AppColors.green
-                          : theme.isDark
-                              ? AppColors.lightPrimary
-                              : AppColors.lightgrey,
-                    ), //Icon(Icons.home),
-                    label: 'Card',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      AppIcons.more,
-                      color: _currentIndex == 4
-                          ? AppColors.green
-                          : theme.isDark
-                              ? AppColors.lightPrimary
-                              : AppColors.lightgrey,
-                    ), //Icon(Icons.home),
-                    label: 'More',
-                  ),
-                ],
-              ),
-              // Stack(
-              //   //   index: selectedIndex,
-              //   children: <Widget>[
-              //     _buildOffstageNavigator('Home'),
-              //     _buildOffstageNavigator('History'),
-              //     _buildOffstageNavigator('Account'),
-              //   ],
-              // ),
-              // bottomNavigationBar: BottomNavigationBar(
-              //   showSelectedLabels: true,
-              //   type: BottomNavigationBarType.fixed,
-              //   showUnselectedLabels: true,
-              //   items: const <BottomNavigationBarItem>[
-              //     BottomNavigationBarItem(
-              //       icon: Icon(
-              //         Icons.home_outlined,
-              //         //color: Colors.grey,
-              //       ),
-              //       label: 'Home',
-              //     ),
-              //     BottomNavigationBarItem(
-              //       icon: Icon(
-              //         Icons.history,
-              //         //color: Colors.grey,
-              //       ),
-              //       label: 'History',
-              //     ),
-              //     BottomNavigationBarItem(
-              //       icon: Icon(
-              //         Icons.person_2_outlined,
-              //         //color: Colors.grey,
-              //       ),
-              //       label: 'Account',
-              //     ),
-              //   ],
-              //   currentIndex: currentIndex,
-              //   unselectedItemColor: Colors.grey,
-              //   selectedIconTheme: const IconThemeData(color: AppColors.green),
-              //   selectedItemColor: AppColors.green,
-              //   onTap: (int index) {
-              //     _onItemTapped(pageyKeys[index], index);
-              //   },
-              // )),
+                    : AppColors.lightgrey,
+              ), //Icon(Icons.home),
+              label: 'Home',
             ),
-          )
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(AppIcons.send,
+                  color: _currentIndex == 1
+                      ? AppColors.green
+                      : theme.isDark
+                      ? AppColors.lightPrimary
+                      : AppColors.lightgrey),
+              label: 'Send',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AppIcons.bill,
+                color: _currentIndex == 2
+                    ? AppColors.green
+                    : theme.isDark
+                    ? AppColors.lightPrimary
+                    : AppColors.lightgrey,
+              ), //Icon(Icons.home),
+              label: 'Bills',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AppIcons.card,
+                color: _currentIndex == 3
+                    ? AppColors.green
+                    : theme.isDark
+                    ? AppColors.lightPrimary
+                    : AppColors.lightgrey,
+              ), //Icon(Icons.home),
+              label: 'Card',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AppIcons.more,
+                color: _currentIndex == 4
+                    ? AppColors.green
+                    : theme.isDark
+                    ? AppColors.lightPrimary
+                    : AppColors.lightgrey,
+              ), //Icon(Icons.home),
+              label: 'More',
+            ),
+          ],
+        ),
+        // Stack(
+        //   //   index: selectedIndex,
+        //   children: <Widget>[
+        //     _buildOffstageNavigator('Home'),
+        //     _buildOffstageNavigator('History'),
+        //     _buildOffstageNavigator('Account'),
+        //   ],
+        // ),
+        // bottomNavigationBar: BottomNavigationBar(
+        //   showSelectedLabels: true,
+        //   type: BottomNavigationBarType.fixed,
+        //   showUnselectedLabels: true,
+        //   items: const <BottomNavigationBarItem>[
+        //     BottomNavigationBarItem(
+        //       icon: Icon(
+        //         Icons.home_outlined,
+        //         //color: Colors.grey,
+        //       ),
+        //       label: 'Home',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(
+        //         Icons.history,
+        //         //color: Colors.grey,
+        //       ),
+        //       label: 'History',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(
+        //         Icons.person_2_outlined,
+        //         //color: Colors.grey,
+        //       ),
+        //       label: 'Account',
+        //     ),
+        //   ],
+        //   currentIndex: currentIndex,
+        //   unselectedItemColor: Colors.grey,
+        //   selectedIconTheme: const IconThemeData(color: AppColors.green),
+        //   selectedItemColor: AppColors.green,
+        //   onTap: (int index) {
+        //     _onItemTapped(pageyKeys[index], index);
+        //   },
+        // )),
+      ),
+    )
         : No_internet_Page(onRetry: _checkConnectivity);
   }
 }
